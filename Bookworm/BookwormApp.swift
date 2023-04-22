e40e1b4022aef854477c5621df0ac2273ef05c31
stevegustason <steve.gustason@gmail.com>
2023-04-22 12:36:16 -0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Steven Gustason on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
